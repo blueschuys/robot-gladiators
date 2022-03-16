4914e42843cc6e0f516ac4860eed522bd30f980a
blueschuys <blueschuys@hotmail.com>
2022-03-15 21:42:33 -0600

    added player messages

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,7 +69,16 @@ var fight = function(enemyName) {
   }; // end of fight function
 
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+    
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
+    debugger;
     fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
